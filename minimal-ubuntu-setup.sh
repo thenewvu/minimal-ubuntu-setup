ec6c702045c42553174d1d9f25986dc6b41e99ad
Vu Le <thenewvu@gmail.com>
2015-10-29 15:24:41 +0700

    Add minimal setup script

---
+++ minimal-ubuntu-setup.sh
@@ -0,0 +1,44 @@
+# turnoff vim swapfile
+echo "set noswapfile" >> ~/.vimrc
+
+# install graphic manager
+sudo apt-get update
+sudo apt-get install xserver-xorg
+
+# setup i3 ppa and install i3
+sudo sh -c 'echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list.d/i3.list'
+sudo apt-get update
+sudo apt-get --allow-unauthenticated install sur5r-keyring
+sudo apt-get update
+sudo apt-get install i3
+
+# install x window manager starter
+sudo apt-get install xinit
+
+# start x window manager (automatically start i3)
+# startx
+
+# setup chrome ppa and install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+# install network manager
+sudo apt-get install wicd-curses
+
+# install utils
+sudo apt-get install htop
+sudo apt-get install tree
+sudo apt-get install software-properties-common
+sudo apt-get install vim
+sudo apt-get install git
+
+# install jdk8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# enable sound
+sudo apt-get install alsa-base gstreamer0.10-alsa
+sudo usermod -a -G audio vu
