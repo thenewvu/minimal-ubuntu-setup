b3f72e91fafbb60b3733abd8cf2a9a2035544017
Vu Le <thenewvu@gmail.com>
2015-10-29 17:01:07 +0700

    Add setup ppa and install inkscape script

--- minimal-ubuntu-setup.sh
+++ minimal-ubuntu-setup.sh
@@ -42,3 +42,8 @@ sudo apt-get install oracle-java8-installer
 # enable sound
 sudo apt-get install alsa-base gstreamer0.10-alsa
 sudo usermod -a -G audio vu
+
+# setup inkscape ppa and install it
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt-get update
+sudo apt-get install inkscape
